0e3c762235f1ef59783e6fb202297b1e99efad50
zain <139786914+zainabugosh@users.noreply.github.com>
2023-07-20 07:29:33 +0300

    lab

--- Advanced-Flask-Lab/app.py
+++ Advanced-Flask-Lab/app.py
@@ -3,16 +3,15 @@ import random
 import requests, json
 
 app = Flask(  # Create a flask app
-    __name__,
+    _name_,
     template_folder='templates',  # Name of html file folder
     static_folder='static'  # Name of directory for static files
 )
 
 # Variables for tasks
-image_link = "https://uploads-ssl.webflow.com/5dd64bd3a930f9d04abd1363/5de254f85f1762feee30d664_meet_logo_red.png"
-
+image_link = "https://www.google.com/imgres?imgurl=https%3A%2F%2Fcdn.britannica.com%2F36%2F167236-050-BF90337E%2FEars-corn.jpg&tbnid=Zc1qE0w9Oop4aM&vet=12ahUKEwifiOSZuZyAAxXepycCHfEaAXwQMygCegUIARCOAg..i&imgrefurl=https%3A%2F%2Fwww.britannica.com%2Fplant%2Fcorn-plant&docid=W1wJVxOqWU0Z7M&w=1600&h=1200&q=corn&ved=2ahUKEwifiOSZuZyAAxXepycCHfEaAXwQMygCegUIARCOAg"
 user_bio = "Middle East Entrepreneurs of Tomorrow. Enabling the next generation of Israeli and Palestinian leaders."
-
+aboutMe = "i am a very smart meet student"
 posts = {
     "https://imageio.forbes.com/blogs-images/samarmarwan/files/2018/03/MEET-Students-1200x800.jpg": "Group projects <3",
     "https://uploads-ssl.webflow.com/5dd64bd3a930f96c82bd137a/63024ce64d943673cb004a4c_2022.07.17%20-%20Summer%20Day%201.png": "MEET summer!",
@@ -25,13 +24,14 @@ posts = {
 
 @app.route('/')  # '/' for the default page
 def home():
-    return render_template('index.html')
+    return render_template('index.html', user_bio = user_bio,
+     image_link=image_link,posts=posts)
 
 
 @app.route('/about')  # '/' for the default page
 def about():
-    return render_template('about.html')
+    return render_template('about.html', aboutMe=aboutMe)
 
 
-if __name__ == "__main__":  # Makes sure this is the main process
+if _name_ == "_main_":  # Makes sure this is the main process
     app.run(debug=True)
